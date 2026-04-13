f8510431d4646ed4a03ae15f4e4c1652a332fc5b
olekkr <olekkr@outlook.com>
2026-04-14 01:32:34 +0200

    remaps

--- init.lua
+++ init.lua
@@ -47,7 +47,17 @@ vim.o.scrolloff = 8
 vim.o.confirm = true
 
 -- [[ Keymaps ]]
+vim.keymap.set('n', 'J', 'mzJ`z')
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- greatest remaps ever
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+vim.keymap.set('x', '<leader>p', [["_dP]])
 
 -- Diagnostic Config & Keymaps
 -- See :help vim.diagnostic.Opts
